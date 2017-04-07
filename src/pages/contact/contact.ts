67215445e3fab1b6aaf198d1c1e425a373705cc7
David Jenkins <david.g.jenkins@nasa.gov>
2017-04-06 23:09:16 -0400

    updated ionic database, worked on quote page, improved scripting, and many other updates

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
-
 import { Database } from '@ionic/cloud-angular';
 
 @Component({
--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Database } from '@ionic/cloud-angular';
 
 @Component({
   selector: 'page-post',
   templateUrl: 'post.html'
 })
-export class PostPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+export class PostPage {
+  public posts: Array<string>;
+  public postsMessage:string = '';
+  public postsTitle:string = '';
+  public postsName:string = '';
+  public myDate:string = '';
+  public postsEstimate:any = '';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public db:Database) {
+  	this.db.connect();
+    this.db.collection('posts').order('created','descending').watch().subscribe( (posts) => {
+      console.dir(posts);
+      this.posts = posts;
+    }, (error) => {
+      console.error(error);
+    });
+
+
+  }
+
+  createPost() {
+    this.db.collection('posts').store({
+    	description:this.postsMessage, 
+    	type:this.postsTitle, 
+    	name:this.postsName, 
+    	date:this.myDate, 
+    	price:this.postsEstimate,
+    	created:Date.now()
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PostPage');

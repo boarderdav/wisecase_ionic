344a8e9a89746d93abde3d2ade3cc50d2cae40ca
davidglennjenkins <djenkins@atlassian.com>
2017-05-22 18:15:22 -0700

    updated platform build for android APK

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-facebook/www/CordovaFacebook.js",
+        "id": "cordova-plugin-facebook.CordovaFacebook",
+        "clobbers": [
+            "CordovaFacebook"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.0.5",
+    "cordova-plugin-device": "1.1.4",
+    "cordova-plugin-facebook": "0.2.2"
+};
+// BOTTOM OF METADATA
+});
